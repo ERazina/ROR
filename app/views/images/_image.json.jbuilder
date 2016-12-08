5c5fbfb1e50182933976a273659c53b06411c43a
Ubuntu <ubuntu@groupone>
2016-12-08 17:25:26 +0000

    added controllers

---
+++ app/views/images/_image.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! image, :id, :name, :qty_items, :created_at, :updated_at
+json.url image_url(image, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  resources :themes
+  resources :images
+  resources :users
+  get 'main/index'
+
+  get 'main/help'
+
+  get 'main/contacts'
+
+  get 'main/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
